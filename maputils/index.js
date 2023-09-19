8640ab1f383f8fe7bf6f8a769cc354d518b9d699
applefishpear <nathanxu1@outlook.com>
2023-09-19 22:48:36 +1000

    add approve/reject calculator

---
+++ maputils/index.js
@@ -0,0 +1,13 @@
+document.getElementById("calcForm").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let ca = parseInt(document.getElementById("ca").value);
+    let a = parseInt(document.getElementById("a").value);
+    let r = parseInt(document.getElementById("r").value);
+    let cr = parseInt(document.getElementById("cr").value);
+
+    document.getElementById("ratio").value = (ca + 0.5 * a) + ":" + (3 + 1.5 * (cr + 0.5 * r));
+    (ca + 0.5 * a) >= (3 + 1.5 * (cr + 0.5 * r))? 
+        document.getElementById("ratio").style.setProperty("color", "rgb(15, 224, 15)") :
+        document.getElementById("ratio").style.setProperty("color", "red");
+});
