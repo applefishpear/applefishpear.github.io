b4fe9260e7bab3e0832c70875f688110158c3bb6
applefishpear <nathanxu1@outlook.com>
2023-11-22 22:21:06 +1100

    added disclaimer to mapfile merger to test mapfile

--- maputils/merger/index.js
+++ maputils/merger/index.js
@@ -3,6 +3,49 @@ let urlForm = document.getElementById("urlForm");
 urlForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
+    // let url1 = String(document.getElementById("url1").value);
+    // let url2 = String(document.getElementById("url2").value);
+
+    // url1 = url1.replace(
+    //     "https://icedodo.onionfist.com/singleplayer?mapUrl=",
+    //     "https://fn.onionfist.com/icemaprun.js?v8="
+    // );
+
+    // url2 = url2.replace(
+    //     "https://icedodo.onionfist.com/singleplayer?mapUrl=",
+    //     "https://fn.onionfist.com/icemaprun.js?v8="
+    // );
+    
+    // console.log(url1);
+    // console.log(url2);
+
+    // fetch(url1) // the URL of the JavaScript file
+    //     .then(res => res.text()) // get the text content of the response
+    //     .then(text => {
+    //         // declare a variable and assign the text to it
+    //         var file1 = text;
+    //         console.log(file1);
+    //         var beautified1 = js_beautify(file1, { indent_size: 4});
+    //         console.log(`beautified: ${beautified1}`);
+    //         list1 = beautified1.split("\\n");
+    //         console.log(list1);
+    // })
+
+    // fetch(url2) // the URL of the JavaScript file
+    //     .then(res => res.text()) // get the text content of the response
+    //     .then(text => {
+    //         // declare a variable and assign the text to it
+    //         var file2 = text;
+    //         console.log(file2);
+    //         var beautified2 = js_beautify(file2, { indent_size: 4});
+    //         console.log(`beautified: ${beautified2}`);
+    //         list2 = beautified2.split("\\n");
+    //         console.log(list2);
+    // })
+
+    // let file1 = list1;
+    // let file2 = list2;
+
     let file1 = String(document.getElementById("url1").value).split("\n");
     let file2 = String(document.getElementById("url2").value).split("\n");
 
